af1a309735067a8328e3cf074885295b70c9b356
marekpol <marekpol@amu.edu.pl>
2017-09-05 11:59:17 +0200

    05.09.2017

--- R/06_breakpoints.R
+++ R/06_breakpoints.R
@@ -17,14 +17,26 @@ dane <- ts(data=dane$poznan_rekonstr, start=1848, end=2016, frequency=1)
 
 ## fit, visualize and test OLS-based CUSUM test
 ## with a Cramer-von Mises functional
-ocus <- efp(dane ~ 1, type = "OLS-CUSUM")
-plot(ocus, functional = "meanL2")
-sctest(ocus, functional = "meanL2")
+
+ocus <- efp(dane ~ 1, type="Rec-CUSUM", h=0.1)
+plot(ocus, boundary=F)
+bound.ocus <- boundary(ocus, alpha=0.05)
+bound.ocus1 <- boundary(ocus, alpha=0.01)
+lines(bound.ocus, col=4);lines(bound.ocus1, col="red")
+#lines(-bound.ocus, col=4)
+sctest(ocus)
+
+ocus <- efp(dane ~ 1, type="OLS-MOSUM", h=0.1)
+plot(ocus, boundary=F)
+bound.ocus <- boundary(ocus, alpha=0.05)
+bound.ocus1 <- boundary(ocus, alpha=0.01)
+lines(bound.ocus, col=4);lines(bound.ocus1, col="red")
 
 
-
+fs <- Fstats(dane ~ 1 , data = dane)
+plot(fs, alpha=0.01)
 ## estimate breakpoints
-bp <- breakpoints(dane ~ 1)
+bp <- breakpoints(dane ~ 1, h=.25)
 plot(bp)
 summary(bp)
 
@@ -107,3 +119,63 @@ plot(ansmean,cpt.col='blue')
 dane2 <- as.data.frame(dane)
 CE.NB(dane2, Nmax = 10,      distyp = 2, penalty = "BIC", parallel = T)
 
+
+## Poznan data with breakpoints. 
+# on the basis of example from package "strucchange"
+
+poznan <- breakpoints(dane ~ 1)
+summary(poznan)
+plot(poznan)
+
+## compute breakdates corresponding to the
+## breakpoints of minimum BIC segmentation
+breakdates(poznan)
+
+## confidence intervals
+ci.poznan <- confint(poznan)
+breakdates(ci.poznan)
+ci.poznan
+
+plot(dane, lwd=.3, ylab = expression("Temperature (" * degree * "C)"))
+lines(ci.poznan)
+
+dane1 <- read_excel("data/xls/calosc_z_imgw.xls")
+dane1 <- dane1 %>% group_by(yy)  %>% summarise_all(., mean)%>%
+                    dplyr::select(yy,poznan_rekonstr) 
+
+trend <- lm(poznan_rekonstr ~yy, dane1)
+summary(trend)
+str(trend)
+lines(ts(fitted(trend), start = 1848), col = 4, lwd=1.7, lty=2)
+
+dane2 <- dane1 %>% group_by(yy)  %>% filter(yy<=1902) %>% summarise_all(., mean)%>%
+  dplyr::select(yy,poznan_rekonstr)
+
+dane3 <- dane1 %>% group_by(yy)  %>% filter(yy>=1902, yy<=1987) %>% summarise_all(., mean)%>%
+  dplyr::select(yy,poznan_rekonstr)
+
+dane4 <- dane1 %>% group_by(yy)  %>% filter(yy>=1987, yy<=2016) %>% summarise_all(., mean)%>%
+  dplyr::select(yy,poznan_rekonstr)
+
+trend2 <- lm(poznan_rekonstr ~yy, dane2)
+trend3 <- lm(poznan_rekonstr ~yy, dane3)
+trend4 <- lm(poznan_rekonstr ~yy, dane4)
+
+summary(trend2)
+summary(trend3)
+summary(trend4)
+
+lines(ts(fitted(trend2), start = 1848), col = 2, lwd=1.5)
+lines(ts(fitted(trend3), start = 1902), col = 2, lwd=1.5)
+lines(ts(fitted(trend4), start = 1987), col = 2, lwd=1.5)
+
+text(1964, 10.4,  expression("" * beta * " = 0.010"), col="blue")
+text(1964, 10.1,  "p = 0.000", col="blue")
+text(1854, 10.4,  expression("" * beta * " = 0.007"), col="red")
+text(1854, 10.1,  "p = 0.247", col="red")
+text(1914, 10.4,  expression("" * beta * " = 0.002"), col="red")
+text(1914, 10.1,  "p = 0.445", col="red")
+text(1995, 10.4,  expression("" * beta * " = 0.045"), col="red")
+text(1995, 10.1,  "p = 0.009", col="red")
+
+#### koniec
